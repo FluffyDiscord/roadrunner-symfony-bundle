6c29941c98c1e452fa20556442d1620d04682955
FluffyDiscord <rostislavkaleta@gmail.com>
2025-02-16 13:53:32 +0100

    clarify the real issue when creating rpc client

--- src/Factory/RPCFactory.php
+++ src/Factory/RPCFactory.php
@@ -14,7 +14,7 @@ class RPCFactory
         $rpc = $_ENV["RR_RPC"] ?? $_SERVER["RR_RPC"] ?? null;
 
         if ($rpc === null) {
-            throw new InvalidRPCConfigurationException("Please enable/configure 'rpc' plugin in your '.rr.yaml'");
+            throw new InvalidRPCConfigurationException("Please set 'RR_RPC=' .env variable and enable 'rpc' plugin in your RoadRunner Yaml configuration, eg '.rr.yaml'");
         }
 
         return RPC::create($environment->getRPCAddress());
